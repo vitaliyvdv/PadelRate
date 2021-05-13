3b6b37b0979e5aeeab2fc8fb768767dce8480cc2
Vit <vit.vdvch@gmail.com>
2021-05-14 02:54:08 +0300

    buttom component

--- src/components/common/button/index.js
+++ src/components/common/button/index.js
@@ -4,17 +4,67 @@ import {StyleSheet, Text, TouchableHighlight, View} from "react-native"
 
 import {Colors, Borders} from "./../../../libraries"
 
-const Icon = ({source}) => {
-  return <View style={styles.icon}>{source}</View>
+const Icon = ({source, style}) => {
+  return <View style={[styles.icon, style]}>{source}</View>
 }
 
-const AppButton = ({onPress, text, color, variant, size, startIcon, endIcon}) => {
+const TextColor = color => {
+  switch (true) {
+    case color == "primary":
+      return {color: Colors.primary}
+      break
+    case color == "secondary":
+      return {color: Colors.light}
+      break
+    default:
+      return {color: Colors.white}
+  }
+}
+
+const TextSize = size => {
+  switch (true) {
+    case size == "sm":
+      return {fontSize: 14, lineHeight: 17}
+      break
+    case size == "lg":
+      return {fontSize: 18, lineHeight: 21}
+      break
+    default:
+      return {fontSize: 16, lineHeight: 19}
+  }
+}
+
+const AppButton = ({onPress, text, color, variant, size, startIcon, endIcon, disabled}) => {
   return (
     <View style={styles.root}>
-      <TouchableHighlight onPress={onPress} color={Colors.white} activeOpacity={0.9}>
-        <View style={[styles.button, styles[size], styles[color]]}>
-          {startIcon && <Icon source={startIcon} />}
-          <Text style={styles.text}>{text}</Text>
+      <TouchableHighlight
+        onPress={onPress}
+        underlayColor={variant == "contained" ? Colors.white : null}
+        activeOpacity={0.9}
+        accessibilityLabel={text}
+        accessibilityRole='button'
+        disabled={disabled}
+      >
+        <View
+          style={[
+            styles.button,
+            variant != "text" && styles[size],
+            variant == "contained" && contained[color],
+            variant == "outlined" && outlined[color]
+          ]}
+        >
+          {startIcon && <Icon style={styles.startIcon} source={startIcon} />}
+          <Text
+            style={[
+              styles.text,
+              TextSize(size),
+              variant == "outlined" && TextColor(color),
+              variant == "text" && TextColor(color),
+              variant == "contained" && {color: Colors.white}
+            ]}
+          >
+            {text}
+          </Text>
           {endIcon && <Icon source={endIcon} />}
         </View>
       </TouchableHighlight>
@@ -35,12 +85,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     alignContent: "center"
   },
-  primary: {
-    backgroundColor: Colors.primary
-  },
-  secondary: {
-    backgroundColor: Colors.light
-  },
   sm: {
     height: 48
   },
@@ -51,15 +95,43 @@ const styles = StyleSheet.create({
     height: 62
   },
   text: {
-    color: Colors.white,
-    fontSize: 16,
-    lineHeight: 19,
     fontWeight: "bold"
   },
   icon: {
     position: "relative",
-    width: 24,
-    height: 24
+    width: 16,
+    height: 16,
+    color: Colors.white
+  },
+  startIcon: {
+    marginRight: 6
+  },
+  endIcon: {
+    marginLeft: 6
+  }
+})
+
+const contained = StyleSheet.create({
+  primary: {
+    backgroundColor: Colors.primary
+  },
+  secondary: {
+    backgroundColor: Colors.light
+  }
+})
+
+const outlined = StyleSheet.create({
+  primary: {
+    backgroundColor: "rgba(255, 255, 255, 0)",
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderColor: Colors.primary
+  },
+  secondary: {
+    backgroundColor: "rgba(255, 255, 255, 0)",
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderColor: Colors.light
   }
 })
 
@@ -68,13 +140,15 @@ AppButton.propTypes = {
   endIcon: PropTypes.node,
   color: PropTypes.oneOf(["primary", "secondary"]),
   size: PropTypes.oneOf(["sm", "md", "lg"]),
-  variant: PropTypes.oneOf(["text", "contained"])
+  variant: PropTypes.oneOf(["text", "contained", "outlined"]),
+  disabled: PropTypes.bool
 }
 
 AppButton.defaultProps = {
   color: "primary",
   variant: "contained",
-  size: "md"
+  size: "md",
+  disabled: false
 }
 
 export default AppButton
